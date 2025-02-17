0210e870fab30d0e4579813f5d80b4e734cc4cf7
a19albertons <a19albertons@iessanclemente.net>
2025-02-17 09:50:50 +0100

    Se ofrece adicionalmente el de antropodos

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,23 @@
+// Lado de la calle
+//fun main() {
+//    val secuencias= generateSequence {
+//        val linea = readln()
+//        if (linea != "0") linea else null
+//    }
+//    secuencias.forEach {
+//       if (it.toInt()%2==0) println("DERECHA") else println("IZQUIERDA")
+//    }
+//}
+
+// Artropodos
 fun main() {
+    val num_casos = readln().toInt()
     val secuencias= generateSequence {
         val linea = readln()
         if (linea != "0") linea else null
-    }
+    }.take(num_casos)
     secuencias.forEach {
-       if (it.toInt()%2==0) println("DERECHA") else println("IZQUIERDA")
+        val (insectos, aracnidos, crustaceos, escolopendras, anillos) = it.split(" ").map { it.toInt() }
+        println(insectos*6+aracnidos*8+crustaceos*10+escolopendras*anillos*2)
     }
 }
