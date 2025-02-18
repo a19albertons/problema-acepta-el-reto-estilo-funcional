cc614b151559603bae1a1ec0906fe0ae86fb51d4
a19albertons <a19albertons@iessanclemente.net>
2025-02-18 09:21:15 +0100

    Se ofrece adicionalmente el de cociendo huevos

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.math.ceil
+import kotlin.math.round
+
 // Lado de la calle
 //fun main() {
 //    val secuencias= generateSequence {
@@ -10,14 +13,27 @@
 //}
 
 // Artropodos
-fun main() {
-    val num_casos = readln().toInt()
+//fun main() {
+//    val num_casos = readln().toInt()
+//    val secuencias= generateSequence {
+//        val linea = readln()
+//        if (linea != "0") linea else null
+//    }.take(num_casos)
+//    secuencias.forEach {
+//        val (insectos, aracnidos, crustaceos, escolopendras, anillos) = it.split(" ").map { it.toInt() }
+//        println(insectos*6+aracnidos*8+crustaceos*10+escolopendras*anillos*2)
+//    }
+//}
+
+// cociendo huevos
+fun main(){
     val secuencias= generateSequence {
-        val linea = readln()
-        if (linea != "0") linea else null
-    }.take(num_casos)
+        val linea= readln()
+        if (linea != "0 0") linea else null
+    }
     secuencias.forEach {
-        val (insectos, aracnidos, crustaceos, escolopendras, anillos) = it.split(" ").map { it.toInt() }
-        println(insectos*6+aracnidos*8+crustaceos*10+escolopendras*anillos*2)
+        val (huevos, capacidad) = it.split(" ").map { it.toDouble() }
+        val veces = huevos/capacidad
+        println(ceil(veces).toInt()*10)
     }
 }
